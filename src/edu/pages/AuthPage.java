7a08054a88269b3289a137f4748a8481d18b7cfb
svetlana-mankut <svetlanamankut@gmail.com>
2015-05-22 17:11:19 +0300

    add forgot pass test

--- src/edu/pages/AuthPage.java
+++ src/edu/pages/AuthPage.java
@@ -1,5 +1,6 @@
 package edu.pages;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,8 +14,17 @@ public class AuthPage {
 	By loginButton = By.name("login");
 	By errorMessage = By.className("errorText");
 	
+	By forgotPass = By.xpath(".//*[@id='js_hide_popup_login']");
+	//form
+	By email = By.className("js_forgot_password_email");
+	By submitButton = By.className("btn");
+	By successMessage = By.className("js_forgot_password_content_2" );
+	By errorMes2 = By.className("errorText");
+	 
 	
-	public AuthPage(FirefoxDriver driver){
+	
+	
+		public AuthPage(FirefoxDriver driver){
 		this.driver = driver;
 	}
 	
@@ -149,12 +159,44 @@ public class AuthPage {
 				}
 
 				}
+//Forgot Password methods
+			
+			
+			public void clickFPlink(){
+				driver.findElement(forgotPass).click();
+				}
+			public void clickSubmit(){
+				driver.findElement(submitButton).click();
+			}
+			public void assertError(){
+				driver.findElement(errorMes2).getText().contains("This is an obligatory field.");
+			}
+			public void setEmail(String strEmail){
+				driver.findElement(email).sendKeys("user@email");
+			}
+			public void assertSuccMessage(){
+				driver.findElement(successMessage).getText().contains("We have just sent temporary password to your email.Use these details to login.");
+			}
+			
+			
+			
+			public void forgotPass (String strEmail){
+				this.clickLogin();
+				this.clickFPlink();
+				this.setEmail(null);
+				this.assertSuccMessage();
+			}
+				
+			public void forgotPassNoEmail(){
+				this.clickLogin();
+				this.clickFPlink();
+				this.clickSubmit();
+				this.assertError();
+			}
 
-				 
-			
-			    
-			    }
-			
+}
+
+					
 		
 	
 	
