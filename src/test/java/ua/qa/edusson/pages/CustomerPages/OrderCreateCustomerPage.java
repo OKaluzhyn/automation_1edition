f136fa9b2b0d9b35bb3dcee25922f0c241c157f4
svetlana-mankut <svetlanamankut@gmail.com>
2016-10-20 16:59:44 +0400

    test

--- src/test/java/ua/qa/edusson/pages/CustomerPages/OrderCreateCustomerPage.java
+++ src/test/java/ua/qa/edusson/pages/CustomerPages/OrderCreateCustomerPage.java
@@ -99,7 +99,7 @@ public class OrderCreateCustomerPage  {
 
 	// click start bidding button
 	public void proceedToBidding() {
-		Helper.sleep(10);
+		//Helper.sleep(10);
 		WebElement bidding_button = app.getHelper().cyclicElementSearchByXpath(startBiddingButton);
 				bidding_button.click();
 	}
@@ -118,7 +118,7 @@ public class OrderCreateCustomerPage  {
 		Helper.sleep(1);
 		this.orderDescription(strDescription);
 		Helper.sleep(1);
-		this.clicUpload();
+	//	this.clicUpload();
 		Helper.sleep(10);
 		this.proceedToBidding();
 	}
