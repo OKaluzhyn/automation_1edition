420258464cc56366b7f4fbcc92dd3f4cd317074d
svetlana-mankut <svetlanamankut@gmail.com>
2016-10-21 13:20:52 +0400

    добавлены файлы properties

--- src/test/java/ua/qa/edusson/tests/TestBase.java
+++ src/test/java/ua/qa/edusson/tests/TestBase.java
@@ -5,19 +5,21 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ua.qa.edusson.utils.ApplicationManager;
 
-import java.util.concurrent.TimeUnit;
+
 
 /**
  * Created by tester on 12.08.2016.
  */
 public class TestBase {
-    public static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+
+   public static final ApplicationManager app
+           = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
+
+
 
     @BeforeMethod
     public void setUp() throws Exception {
         app.init();
-        app.driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
-        app.driver.manage().timeouts().implicitlyWait(1200, TimeUnit.SECONDS);
     }
 
     @AfterMethod
--- src/test/java/ua/qa/edusson/utils/ApplicationManager.java
+++ src/test/java/ua/qa/edusson/utils/ApplicationManager.java
@@ -6,30 +6,46 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by tester on 12.08.2016.
  */
 public class ApplicationManager {
-    public WebDriver driver;
+    public  WebDriver driver;
+
+    private final Properties properties;
     private String browser;
     private Helper helper;
 
-    public ApplicationManager(String browser) {
-              this.browser = browser;
-          }
+    public ApplicationManager(String browser){
+        this.browser = browser;
+        properties = new Properties();
+
+    }
 
 
-    public void init(){
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             driver = new FirefoxDriver();
         } else if (Objects.equals(browser, BrowserType.CHROME)) {
-            driver = new ChromeDriver();;
-        } else if (Objects.equals(browser, BrowserType.IE)){
+            driver = new ChromeDriver();
+            ;
+        } else if (Objects.equals(browser, BrowserType.IE)) {
             driver = new InternetExplorerDriver();
         }
+        driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        driver.get(properties.getProperty("site"));
         helper = new Helper(driver);
     }
 
