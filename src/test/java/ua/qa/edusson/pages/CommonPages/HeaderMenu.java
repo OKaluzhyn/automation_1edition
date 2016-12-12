51327b05f4b68dd8736f76758416780467eff15b
svetlana-mankut <svetlanamankut@gmail.com>
2016-12-12 13:42:56 +0300

    logout унифицирован для всех сайтов, включительно с сайтами где кнопка находится в дропдауне

--- src/test/java/ua/qa/edusson/pages/CommonPages/HeaderMenu.java
+++ src/test/java/ua/qa/edusson/pages/CommonPages/HeaderMenu.java
@@ -8,15 +8,29 @@ public class HeaderMenu {
     public static String logOut2 = "//a[text()='Log out']";
     public static String userMenu = "//div[@class='block__user_controllers']";
 
-    public void userLogOut() {
-        if (app.getHelper().isElementPresent("//div[@class='block__user_controllers']")) {
-            app.getHelper().cyclicElementSearchByXpath(userMenu).click();
 
-            app.getHelper().waitElement(logOut2);
-            app.getHelper().cyclicElementSearchByXpath(logOut2).click();
-        } else {
-            app.getHelper().cyclicElementSearchByXpath(logOutMain).click();
+    public void userLogOut() {
+        String site = app.driver.getCurrentUrl();
+       // System.out.println(site);
+        boolean found = false;
+
+        String[] sitesWithDropHeaderMenu = {
+                "http://essayvikings.com/", "http://eduzaurus.com/", "http://studarea.com/", "http://essaybison.com/",
+                "http://samedaypapers.com/", "http://studyfaq.com/", "http://australianwritings.com.au/", "http://papercp.com/"};
+        for (String i : sitesWithDropHeaderMenu) {
+           // System.out.println(i);
+            if (i.startsWith(site)) {
+                found = true;
+                break;
+            }
+        }
+        if (found){
+                app.getHelper().cyclicElementSearchByXpath(userMenu).click();
+                app.getHelper().waitElement(logOut2);
+                app.getHelper().cyclicElementSearchByXpath(logOut2).click();
+            } else {
+                app.getHelper().cyclicElementSearchByXpath(logOutMain).click();
+            }
 
         }
     }
-}
